c5dbbe7e822b3754bc97500893f94ec5ccf05dbe
HPhucMG <nasusdianguchaizz@gmail.com>
2024-10-22 04:51:13 +0700

    update main lan 3

--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:musicapp/common/widgets/button/basic_button_app.dart';
-import 'package:musicapp/core/configs/assets/app_images.dart';
-import 'package:musicapp/core/configs/assets/app_vectors.dart';
+
+import '../../../common/widgets/button/basic_button_app.dart';
+import '../../../core/configs/assets/app_images.dart';
+import '../../../core/configs/assets/app_vectors.dart';
+import '../../../core/configs/theme/app_colors.dart';
+import '../../choose_mode/pages/choose_mode.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -16,7 +19,16 @@ class GetStartedPage extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
             decoration: const BoxDecoration(
                 image: DecorationImage(
-                    fit: BoxFit.fill, image: AssetImage(AppImages.introBG))),
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AppImages.introBG,
+                    ))),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
             child: Column(
               children: [
                 Align(
@@ -25,11 +37,11 @@ class GetStartedPage extends StatelessWidget {
                 ),
                 const Spacer(),
                 const Text(
-                  'Enjoy Listening to music',
+                  'Enjoy Listening To Music',
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                      color: Colors.white),
+                      color: Colors.white,
+                      fontSize: 18),
                 ),
                 const SizedBox(
                   height: 21,
@@ -37,22 +49,26 @@ class GetStartedPage extends StatelessWidget {
                 const Text(
                   'It is important to take care of the patient, to be followed by the patient, but it will happen at such a time that there is a lot of work and pain.',
                   style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 13,
-                    color: Colors.grey,
-                  ),
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.grey,
+                      fontSize: 13),
                   textAlign: TextAlign.center,
                 ),
                 const SizedBox(
                   height: 20,
                 ),
-                BasicAppButton(onPressed: () {}, title: 'Get started')
+                BasicAppButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  const ChooseModePage()));
+                    },
+                    title: 'Get Started')
               ],
             ),
           ),
-          Container(
-            color: Colors.black.withOpacity(0.15),
-          )
         ],
       ),
     );
